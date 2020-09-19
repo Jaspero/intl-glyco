c159e24e2a644dcb7d6e8b60130bad1b13172570
sven <sven.djanis@gmail.com>
2020-09-19 19:56:28 +0200

    chore(workflow): working on deployment

--- client/projects/cms/src/env-config.ts
+++ client/projects/cms/src/env-config.ts
@@ -1,12 +1,12 @@
 export const ENV_CONFIG = {
   firebase: {
-    apiKey: "AIzaSyCPEUOs08VuV0jlc7LnkUh61x5-TdyGiXU",
-    authDomain: "intl-glyco-cb2de.firebaseapp.com",
-    databaseURL: "https://intl-glyco-cb2de.firebaseio.com",
-    projectId: "intl-glyco-cb2de",
-    storageBucket: "intl-glyco-cb2de.appspot.com",
-    messagingSenderId: "348954630496",
-    appId: "1:348954630496:web:1d03e42398099f183ea605",
-    measurementId: "G-8BN9S54RZN"
+    apiKey: 'AIzaSyCPEUOs08VuV0jlc7LnkUh61x5-TdyGiXU',
+    authDomain: 'intl-glyco-cb2de.firebaseapp.com',
+    databaseURL: 'https://intl-glyco-cb2de.firebaseio.com',
+    projectId: 'intl-glyco-cb2de',
+    storageBucket: 'intl-glyco-cb2de.appspot.com',
+    messagingSenderId: '348954630496',
+    appId: '1:348954630496:web:1d03e42398099f183ea605',
+    measurementId: 'G-8BN9S54RZN'
   }
 };
