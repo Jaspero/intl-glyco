2fbd20c0144fa045c00d6b59460362328b16a527
Kole <h.kolaric92@gmail.com>
2020-09-22 16:44:46 +0200

    feat(fronted): jp propagation added and lint error fixed

--- client/projects/website/src/app/modules/events/events.component.spec.ts
+++ client/projects/website/src/app/modules/events/events.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EventsComponent } from './events.component';
 
@@ -6,12 +6,12 @@ describe('EventsComponent', () => {
   let component: EventsComponent;
   let fixture: ComponentFixture<EventsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ EventsComponent ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EventsComponent);
--- client/projects/website/src/app/modules/page/page.component.spec.ts
+++ client/projects/website/src/app/modules/page/page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageComponent } from './page.component';
 
@@ -6,12 +6,12 @@ describe('PageComponent', () => {
   let component: PageComponent;
   let fixture: ComponentFixture<PageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ PageComponent ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PageComponent);
